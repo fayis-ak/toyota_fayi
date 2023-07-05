f4e6c823349bdeee7bb1fb9c5602e7806b1ea9d1
MUHAMMED FAYIS AK <137692326+fayis-ak@users.noreply.github.com>
2023-07-05 11:31:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        },
+       
+      
+    }
+  })
+
+//  alert("Page note Responding")
+
+
+alert("this is my sample page");
+
+
+
+
+
+function myFunction() {
+  alert("I am an alert box!");
+}
+
+
